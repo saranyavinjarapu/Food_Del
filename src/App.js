874594873fa99d6ef198f449c8447ee53acb8b76
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2024-04-29 15:44:17 +0530

    setup routes for various pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
